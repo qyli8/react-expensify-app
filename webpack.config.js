9a4db79a71cc93197ea089dfd8a32633496d01ff
yuyujo <qyli8@student.monash.edu>
2019-06-30 22:44:31 +1000

    setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env)=>{
   return({
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -44,7 +44,8 @@ module.exports = (env)=>{
     devServer: {
       // server up bundle file in memory rather than generate a physical file - more efficient and faster
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   
   }
