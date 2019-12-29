552630f5389cfe0ed159e79dc567747cee1fcc50
yuyujo <qyli8@student.monash.edu>
2019-12-29 20:40:46 +1100

    formatting

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,28 +1,28 @@
-export const login=(status)=>({
-  type:'LOGED_IN',
+export const login = (status) => ({
+  type: 'LOGED_IN',
   status
 })
 
-export const logout=(status)=>({
-  type:'LOGED_OUT',
+export const logout = (status) => ({
+  type: 'LOGED_OUT',
   status
 })
 
-export const sessionLogin =(status)=> {
-  
+export const sessionLogin = (status) => {
+
   // Invert control!
   // Return a function that accepts `dispatch` so we can dispatch later.
   // Thunk middleware knows how to turn thunk async actions into actions.
-  
- 
-  return (dispatch ) => {
-    Promise.all([localStorage.setItem("loginStatus", status)]).then(()=>dispatch(login(status))).then(()=>{console.log("login DONE!")})
+
+
+  return (dispatch) => {
+    Promise.all([localStorage.setItem("loginStatus", status)]).then(() => dispatch(login(status))).then(() => { console.log("login DONE!") })
   };
-  
+
 }
 
-export const clearSession=(status)=>{
-  return(dispatch)=>{
-    Promise.all([localStorage.removeItem("loginStatus")]).then(()=>dispatch(logout(status)))
+export const clearSession = (status) => {
+  return (dispatch) => {
+    Promise.all([localStorage.removeItem("loginStatus")]).then(() => dispatch(logout(status)))
   }
 }
